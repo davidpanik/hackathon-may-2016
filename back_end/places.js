84bb62b6ce636e7d44b5f008e2d3c99ea1e41c1a
ljelewis <luke@LukeBook.local>
2016-05-21 15:35:47 +0100

    include country in place get

--- back_end/places.js
+++ back_end/places.js
@@ -14,17 +14,34 @@ module.exports = function(lat, lon, params, callback) {
   var radarSearch = googlePlaces.radarSearch;
 	var placeDetailsRequest = googlePlaces.placeDetailsRequest;
 
-  var detailsRequest = function(reference, detailCallback) {
+  var detailsRequest = function(reference, iteration, detailCallback) {
   	var detailsDeferred = Q.defer();
 
     placeDetailsRequest({reference: reference}, function (error, response) {
         if (error) throw error;
 
-        detailCallback(response);
+        detailCallback(response, iteration);
     });
 
     return detailsDeferred;
   }
+
+ 	var getPrimaryPlaceInfo = function(placeDetails) {
+ 		if(!placeDetails.address_components || !placeDetails.address_components.length) {
+ 			return {};
+ 		}
+
+ 		for (var i = 0;i < placeDetails.address_components.length; i++) {
+ 			for(var j = 0; j < placeDetails.address_components[i].types.length; j++) {
+ 				if(placeDetails.address_components[i].types[j] === 'country') {
+
+ 					return placeDetails.address_components[i];
+ 				}
+ 			}
+ 		};
+
+ 		return {};
+ 	}
 
   var parameters = {
       location: [lat, lon],
@@ -35,20 +52,28 @@ module.exports = function(lat, lon, params, callback) {
   radarSearch(parameters, function (error, response) {
     if (error) throw error;
 
-    var formatted = [];
+    var formatted = [],
+    	primaryPlace = {},
+    	count = 0,
+     	i;
 
-    for (var i = 0; i < 10; i++) {
-
+    for (i = 0; i < 10; i++) {
     	if(!response.results[i]) {
     		continue;
     	}
 
-    	detailsRequest(response.results[i].reference, function(details) {
-
+    	detailsRequest(response.results[i].reference, i, function(details, iteration) {
+    		count += 1;
     		formatted.push(details.result);
 
-    		if(i === 10) {
-		      callback(formatted);
+    		if(iteration === 0) {
+    			// fill primary place
+    			primaryPlace = getPrimaryPlaceInfo(details.result);
+    			console.log(primaryPlace);
+    		}
+
+    		if(count === 10) {
+		      callback(formatted, primaryPlace);
     		}
     	});
     };
